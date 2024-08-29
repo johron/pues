72ac980d198c00ea469a77ff632b143349e81423
johron <johron12@icloud.com>
2024-08-29 18:01:18 +0200

    forgot git add .

---
+++ src/command/exec.lua
@@ -0,0 +1,14 @@
+--[[ 
+ - This file is part of Pues (https://github.com/johron/pues/).
+ - Copyright (c) 2024 Johan Rong.
+ -
+ - This source code is subject to the terms of the GNU General Public
+ - License, version 3. If a copy of the GPL was not distributed with this
+ - file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ --]]
+
+---Execute either build or run
+---@param mode string|nil build|run|nil
+return function(mode)
+    print("TODO: implement this")
+end

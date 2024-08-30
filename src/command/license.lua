77a971a876a03bde925c91baf28a881910f3c3fd
johron <johron12@icloud.com>
2024-08-30 18:52:53 +0200

    add license command

---
+++ src/command/license.lua
@@ -0,0 +1,15 @@
+--[[ 
+ - This file is part of Pues (https://github.com/johron/pues/).
+ - Copyright (c) 2024 Johan Rong.
+ -
+ - This source code is subject to the terms of the GNU General Public
+ - License, version 3. If a copy of the GPL was not distributed with this
+ - file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ --]]
+
+---License a project
+---@param arg table
+return function(arg)
+    -- pues license <license> [<author>] [<year>]
+    print("TODO: implement license command")
+end

90b88b1dfeb794835e0be4a41b442d6c1800347a
Johan Rong <59508244+johron@users.noreply.github.com>
2024-09-04 15:09:11 +0200

    scm-1

--- pues/main.lua
+++ pues/main.lua
@@ -9,7 +9,7 @@
 
 require("pues.util.io")
 
-Version = "1.0-2"
+Version = "scm-1"
 PuesPath = string.format("%s/.pues/", os.getenv("HOME"))
 
 if not io.exists(PuesPath) or io.is_dir_empty(PuesPath) then

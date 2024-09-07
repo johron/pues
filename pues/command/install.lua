c8203f9109091803c4179e297764227660cba2fc
Johan Rong <59508244+johron@users.noreply.github.com>
2024-09-07 23:04:05 +0200

    Dependencies are now implemented

--- pues/command/install.lua
+++ pues/command/install.lua
@@ -39,23 +39,43 @@ return function(arg)
         os.exit(1)
     end
 
-    for i, v in pairs(dependencies) do
-        --print(i, json.encode(v))
+    print("Installing dependencies..")
 
+    for i, v in pairs(dependencies) do
         local command = v["command"]
         local packages = v["packages"]
 
         if not command or #command == 0 then
-            print("pues: missing command property for the '%s' package manager in dependencies part of point", i)
+            printf("pues: missing command property for the '%s' package manager in dependencies part of point", i)
             os.exit(1)
         end
 
         if not packages then
-            print("pues: missing packages property for the '%s' package manager in dependencies part of point", i)
+            printf("pues: missing packages property for the '%s' package manager in dependencies part of point", i)
             os.exit(1)
         end
 
-        print(command)
-        print(json.encode(packages))
+        for _, pkg in ipairs(packages) do
+            local handle = io.popen(command .. " " .. pkg .. " 2>&1")
+            if handle == nil then
+                printf("pues: error installing '%s' from '%s'", pkg, i)
+                os.exit(1)
+            end
+
+            local result = handle:read("*a")
+            local success, exit_Type, exit_code = handle:close()
+
+            if (result and result ~= "") and (exit_code == 1 or success ~= true) then
+                local cleaned = result:gsub("\n$", ""):gsub("^\n", "")
+                if cleaned ~= nil then
+                    printf("pues: error installing '%s' from '%s':", pkg, i)
+                    print(cleaned)
+                end
+
+                os.exit(1)
+            end
+        end
     end
+
+    print("Finished installing dependencies")
 end
